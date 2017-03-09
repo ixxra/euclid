9895a7f5c9c745a0f6f0f31d0d1d1bc9a2bd6239
ixxra <garciamx@gmail.com>
2017-03-09 16:36:08 -0600

    first commit

---
+++ src/scripts/euclid.js
@@ -0,0 +1,44 @@
+(function (document, window) {
+	function euclid(a, b) {
+		if (a < b) {
+			var temp = a;
+			a = b;
+			b = temp;
+		}
+		var q = Math.floor(a / b);
+		var r = a % b;
+
+		var steps = [{a: a, b: b, q: q, r: r}];
+		while (r > 0) {
+			a = b;
+			b = r;
+			q = Math.floor(a / b);
+			r = a % b;
+			steps.push({a: a, b: b, q: q, r: r})
+		}
+		return steps;
+	}
+
+	function toHtml(steps){
+		return steps.map(function(step){
+			return `<div class="displayed"><div class="eqn"><span>${step.a}</span> = <span>${step.b}</span> x <span>${step.q}</span> + <span>${step.r}</span></div></div>`
+		}).join('\n');
+	}
+
+	function setup(input_value1, input_value2, solver_form, solution_elm, mcd_elm){
+		solver_form.addEventListener('submit', function (evt) {
+			evt.preventDefault();
+			var a = parseInt(input_value1.value);
+			var b = parseInt(input_value2.value);
+			var steps = euclid(a, b);
+			solution_elm.innerHTML = toHtml(steps);
+			mcd_elm.innerHTML = `GCD: ${steps[steps.length - 1]['b']}`;
+		});
+	}
+
+	window.Euclid = {
+		solver: euclid,
+		setup: setup
+	};
+})(document, window);
+
